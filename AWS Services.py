82671aa1e358d5f6ea92ea988b36120a14bf0ba1
EC2 Default User <ec2-user@ip-172-31-4-83.ec2.internal>
2022-06-25 10:52:07 +0000

    aws_service list done

--- AWS Services.py
+++ AWS Services.py
@@ -1,31 +1,44 @@
-#Create a list of services using Python. IE: S3, Lambda, EC2, etc
-
+#Create a list of aws services using Python.
 
 # The list should be empty initially
 
-AWS_Services = []
-
-print(AWS_Services)
-
-# Populate the list using append or insert.
+aws_services = [ ]
+print(aws_services)
+print(len(aws_services))
 
 
+# Populate the list using insert and append.
 
-
+aws_services.insert(0, 'CloudFormation')
+aws_services.insert(1, 'CloudFront')
+aws_services.append('S3')
+aws_services.append('DynamoDB')
+aws_services.append('VPC')
 
 
 # Print the list and the length of the list.
 
+print(aws_services)
+print(len(aws_services))
 
 
+# Remove two specific services from the list by index and by name.
 
-
-
-# Remove two specific services from the list by name or by index.
-
-
-
-
+del aws_services[0] 
+aws_services.remove('CloudFront')
 
 
 # Print the new list and the new length of the list.
+
+print(aws_services)
+print(len(aws_services))
+
+
+# Add the two services back into list.
+
+aws_services += ['EC2', 'Cloud9']
+
+# Print the list and the length of the list to verify services where added back.
+
+print(aws_services)
+print(len(aws_services))
