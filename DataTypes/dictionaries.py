93026ee3cb26bdecd7db1d180cc992547e122b28
EC2 Default User <ec2-user@ip-172-31-4-83.ec2.internal>
2022-07-06 13:01:48 +0000

    using dictionaries

---
+++ DataTypes/dictionaries.py
@@ -0,0 +1,19 @@
+# creating dictionary
+user = {"first_name":"Dylan"}
+print(user)
+
+
+# reading dictionay
+user = {"first_name":"Dylan"}
+print(user["first_name"])
+
+
+# update
+user["family_name"] = "Knight"              # add/modify a key-value
+print(user)
+    
+
+# delete a key-value pair
+del user["family_name"]
+print(user)
+
