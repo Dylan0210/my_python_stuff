064231a3d4c9818186d0b40da9ff243ac14428af
Dylan0210 <dylanknight0210@gmail.com>
2022-07-08 18:56:47 +0000

    adding instances for project

---
+++ LUIT projects/week 14/create-multiple-instances.py
@@ -0,0 +1,40 @@
+import boto3
+
+
+ec2 = boto3.resource('ec2')
+
+# create 3 intances for PROD
+prod = ec2.create_instances(
+    ImageId='ami-0cff7528ff583bf9a',
+    InstanceType='t2.micro',
+    MaxCount= 3,
+    MinCount= 3,
+    TagSpecifications=[
+        {
+            'ResourceType': 'instance',
+            'Tags': [{'Key': 'Name','Value': 'PROD'},
+                {'Key': 'ENV','Value': 'PROD'}
+            ]
+        }
+    ]
+)
+
+print('PROD Instances:'+prod)
+    
+# create 3 instances for DEV
+dev = ec2.create_instances(
+    ImageId='ami-0cff7528ff583bf9a',
+    InstanceType='t2.micro',
+    MaxCount= 3,
+    MinCount= 3,
+    TagSpecifications=[
+        {
+            'ResourceType': 'instance',
+            'Tags': [{'Key': 'Name','Value': 'DEV'},
+                {'Key': 'ENV','Value': 'DEV'}
+            ]
+        }
+    ]
+)
+
+print('DEV Instances:'+dev)
