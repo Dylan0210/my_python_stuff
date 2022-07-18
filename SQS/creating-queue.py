1c5c7d2fb0b238883e8dcfb93aab57072b322fe7
Dylan0210 <dylanknight0210@gmail.com>
2022-07-18 03:02:44 +0000

    creating queue

---
+++ SQS/creating-queue.py
@@ -0,0 +1,10 @@
+import boto3
+
+# calling the resource
+sqs = boto3.resource('sqs')
+
+# creating the queue
+queue = sqs.create_queue(QueueName='Time')
+
+# printing out the queue url
+print(queue.url) 
